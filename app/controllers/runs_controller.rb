44903cba12a9c974d4252e54af73e230e978ce93
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-05-20 19:24:10 +0900

    Runsコントローラーと各ビューファイルの記述

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,4 +1,25 @@
 class RunsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @runs = Run.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @run = Run.new
+  end
+
+  def create
+    @run = Run.new(run_params)
+    if @run.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def run_params
+    params.require(:run).permit(:title, :content, :image).merge(user_id: current_user.id)
   end
 end
