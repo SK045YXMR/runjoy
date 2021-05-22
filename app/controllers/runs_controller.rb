672bdc2a6531740f2d7ab120be6901c22c4ff6f0
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-05-22 10:12:11 +0900

    runsコントローラーの編集

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,5 +1,6 @@
 class RunsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @runs = Run.includes(:user).order("created_at DESC")
@@ -18,8 +19,32 @@ class RunsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @run.update(run_params)
+      redirect_to run_path(@run.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @run.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def run_params
     params.require(:run).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @run = Run.find(params[:id])
   end
 end
